d87885708d6c434ca181a8cbf2ad73c9adc9e57f
thuannguyen89 <235thuan@gmail.com>
2019-04-26 15:50:27 +0700

    nop bai lan 1

---
+++ tinhluyentapifesle.js
@@ -0,0 +1,25 @@
+var tendangnhap= prompt("ai day", '');
+
+if (tendangnhap == "admin") {
+    alert("Chao mung");
+}
+    else if (tendangnhap == null) {
+    alert ("Vui long nhap ten" );
+}
+    else {
+    alert("toi khong biet ban"); matkhau = false
+}
+
+var matkhau= prompt ("nhap tu khoa", '');
+
+    if (matkhau == "hoanthanh") {
+        alert("Dang nhap thanh cong");
+    }
+    else if (matkhau == null) {
+        alert ("Vui long nhap tu khoa");
+    }
+    else {
+        alert ("Ban nhap sai roi");
+    }
+
+
